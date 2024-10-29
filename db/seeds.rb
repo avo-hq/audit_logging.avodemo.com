fceb9056ba9a59d44b69841cb867c269e2c3ce7a
Paul Bob <paul.ionut.bob@gmail.com>
2024-10-29 11:27:22 +0200

    basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,111 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# AVO_ADMIN_PASSWORD=secret bin/rails db:seed
+
+def progress_params(total:, title:)
+  {
+    total: total,
+    format: "\e[32m%t : %B %p%% %c/%C [ %a ]\e[0m",
+    progress_mark: "=",
+    length: 90,
+    starting_at: 0,
+    title: " #{title}",
+    title_width: 20,
+    throttle_rate: 0.01,
+    remainder_mark: "."
+  }
+end
+
+# ---------------------------------- USERS ---------------------------------- #
+User.delete_all
+
+famous_users = [
+  {
+    first_name: "David Heinemeier",
+    last_name: "Hansson",
+    email: "david@hey.com"
+  },
+  {
+    first_name: "Chris",
+    last_name: "Oliver",
+    email: "chris@gorails.com"
+  },
+  {
+    first_name: "Jason",
+    last_name: "Charnes",
+    email: "jason@jasoncharnes.com"
+  },
+  {
+    first_name: "Jason",
+    last_name: "Swett",
+    email: "jason@benfranklinlabs.com"
+  },
+  {
+    first_name: 'Yukihiro "Matz"',
+    last_name: "Matsumoto",
+    email: "matz@ruby.or.jp"
+  },
+  {
+    first_name: "Joe",
+    last_name: "Masilotti",
+    email: "joe@masilotti.com"
+  },
+  {
+    first_name: "Lucian",
+    last_name: "Ghinda",
+    email: "lucian@ghinda.com"
+  },
+  {
+    first_name: "Mike",
+    last_name: "Perham",
+    email: "mperham@gmail.com"
+  },
+  {
+    first_name: "Taylor",
+    last_name: "Otwell",
+    email: "taylor@laravel.com"
+  },
+  {
+    first_name: "Adam",
+    last_name: "Watham",
+    email: "adam@adamwathan.me"
+  },
+  {
+    first_name: "Jeffery",
+    last_name: "Way",
+    email: "jeffrey@laracasts.com"
+  },
+  {
+    first_name: "Adrian",
+    last_name: "Marin",
+    email: "adrian@adrianthedev.com"
+  }
+]
+
+random_users_number = 19
+
+users_progress_bar = ProgressBar.create(
+  progress_params(total: famous_users.size + random_users_number + 1, title: "Creating users")
+)
+
+users = []
+random_users_number.times do |i|
+  users.push FactoryBot.create(:user)
+  users_progress_bar.increment
+end
+
+users.push User.create(
+  first_name: "Avo",
+  last_name: "Cado",
+  email: "avo@cado.com",
+  password: (ENV["AVO_ADMIN_PASSWORD"] || :secret),
+  roles: {
+    admin: true,
+    manager: false,
+    writer: false
+  }
+)
+users_progress_bar.increment
+
+famous_users.reverse_each do |user|
+  users.push(FactoryBot.create(:user, **user))
+  users_progress_bar.increment
+end
